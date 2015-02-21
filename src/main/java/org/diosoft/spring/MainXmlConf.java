dcd7a9250993f84f43dc88dd94fa91600da4e0cc
wasman <kislo.metal@gmail.com>
2015-02-21 18:45:34 +0200

    Alias

--- src/main/java/org/diosoft/spring/MainXmlConf.java
+++ src/main/java/org/diosoft/spring/MainXmlConf.java
@@ -2,11 +2,17 @@ package org.diosoft.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class MainXmlConf {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
         applicationContext.registerShutdownHook();
+        String[] aliases = applicationContext.getAliases("helloService");
+        System.out.println("All aliases" + Arrays.toString(aliases));
+        Object serviceAlias = applicationContext.getBean("helloServiceAlias1");
+        System.out.printf("Actual class " + serviceAlias.getClass().getName());
     }
 
 }
